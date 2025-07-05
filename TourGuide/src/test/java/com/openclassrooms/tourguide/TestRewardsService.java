69b1001cf9bb48d15af094edd4a43c7e39aa0ce0
watch-me-fly <thi.phoned@gmail.com>
2025-07-06 00:34:27 +0200

    using mockito in TestRewardsService() + adapting userGetRewards()

--- TourGuide/src/test/java/com/openclassrooms/tourguide/TestRewardsService.java
+++ TourGuide/src/test/java/com/openclassrooms/tourguide/TestRewardsService.java
@@ -2,18 +2,27 @@ package com.openclassrooms.tourguide;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 import gpsUtil.location.Location;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import gpsUtil.GpsUtil;
 import gpsUtil.location.Attraction;
 import gpsUtil.location.VisitedLocation;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import rewardCentral.RewardCentral;
 import com.openclassrooms.tourguide.helper.InternalTestHelper;
 import com.openclassrooms.tourguide.service.RewardsService;
@@ -21,23 +30,57 @@ import com.openclassrooms.tourguide.service.TourGuideService;
 import com.openclassrooms.tourguide.user.User;
 import com.openclassrooms.tourguide.user.UserReward;
 
+@ExtendWith(MockitoExtension.class)
 public class TestRewardsService {
+
+	@Mock
+	private GpsUtil gpsUtil;
+	@Mock
+	private RewardCentral rewardCentral;
+	@InjectMocks
+	private RewardsService rewardsService;
+
+	private User user;
+	private List<Attraction> attractions;
+
+	@BeforeEach
+	public void setup() {
+		MockitoAnnotations.openMocks(this);
+
+		user = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
+		attractions = List.of(
+				new Attraction("A", "City", "State", 10.0, 10.0),
+				new Attraction("B", "City", "State", 20.0, 20.0),
+				new Attraction("C", "City", "State", 30.0, 30.0),
+				new Attraction("D", "City", "State", 40.0, 40.0),
+				new Attraction("E", "City", "State", 50.0, 50.0));
+
+		rewardsService = new RewardsService(gpsUtil, rewardCentral);
+		when(gpsUtil.getAttractions()).thenReturn(attractions);
+		when(rewardCentral.getAttractionRewardPoints(any(), any())).thenReturn(500);
+	}
 
 	@Test
 	public void userGetRewards() {
-		GpsUtil gpsUtil = new GpsUtil();
-		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
-
 		InternalTestHelper.setInternalUserNumber(0);
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
 
-		User user = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
-		Attraction attraction = gpsUtil.getAttractions().get(0);
+		// location
+		Location mockedLocation = new Location(10.0, 10.0);
+		VisitedLocation mockedVisit = new VisitedLocation(user.getUserId(), mockedLocation, new Date());
+		when(gpsUtil.getUserLocation(user.getUserId())).thenReturn(mockedVisit);
+
+		// attraction
+		Attraction attraction = attractions.getFirst();
 		user.addToVisitedLocations(new VisitedLocation(user.getUserId(), attraction, new Date()));
+
+		// act
 		tourGuideService.trackUserLocation(user);
+
 		List<UserReward> userRewards = user.getUserRewards();
+
 		tourGuideService.tracker.stopTracking();
-		assertTrue(userRewards.size() == 1);
+        assertEquals(1, userRewards.size());
 	}
 
 	@Test
@@ -56,8 +99,6 @@ public class TestRewardsService {
 	@Test
 	public void nearAllAttractions() {
 		// Arrange ___
-		GpsUtil gpsUtil = new GpsUtil();
-		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
 		List<Attraction> attractions = gpsUtil.getAttractions();
 
 		// Sets proximity to MAX, so the user is near EVERY attraction
@@ -65,7 +106,6 @@ public class TestRewardsService {
 		// Sets the test to simulate 1 user
 		InternalTestHelper.setInternalUserNumber(1);
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
-		User user = tourGuideService.getAllUsers().get(0);
 
 		// Act ___
 		// Add a visited location at each attraction location
